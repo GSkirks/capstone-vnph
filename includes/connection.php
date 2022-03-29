60eb70222cb1cdaf93719d53e4e0c8878753a38a
Kimpoy Marana <87972737+GSkirks@users.noreply.github.com>
2022-03-29 11:59:33 +0800

    connection

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
- $db = mysqli_connect('localhost', 'root', '') or
+ $db = mysqli_connect('remotemysql.com', 'Sj8WSEw2T2', '5U8tgYByzz') or
         die ('Unable to connect. Check your connection parameters.');
-        mysqli_select_db($db, 'reservevision' ) or die(mysqli_error($db));
+        mysqli_select_db($db, 'Sj8WSEw2T2' ) or die(mysqli_error($db));
 ?>
